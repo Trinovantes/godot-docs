b2dc835e6e5ad4099cf287bbc9ad96f348a0320d
Stephen Li <stephenli.ca@gmail.com>
2025-09-11 08:39:04 -0400

    Move docs submodule to under libs

--- src/Constants.ts
+++ src/Constants.ts
@@ -3,6 +3,6 @@ export const ALGOLIA_SEARCH_API_KEY = '92ba993c8ad6a6310c7f468ea46a82a7'
 export const SEARCH_INDEX = 'godot-docs'
 
 // Assume node.js is executed from root of project
-export const RST_DIR = './docs'
+export const RST_DIR = './libs/godot-docs'
 export const MARKDOWN_DIR = './.cache'
 export const BASE_PATH = '/godot-docs/'
